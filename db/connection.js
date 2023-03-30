58a587e6c2f74891ba1dc745f00d909d5c03c22c
nicola <you@example.com>
2023-03-30 12:55:18 +0100

    settings for production added

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+const config = ENVE === 'production'?{
+  connectionString: process.env.DATABASE_URL,
+  max:2,
+}
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -9,4 +16,4 @@ if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
